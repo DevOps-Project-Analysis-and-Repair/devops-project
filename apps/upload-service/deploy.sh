6ffbf9d459929146e4342718be14f14d21fa3bff
Joey de Ruiter <contact@joeyderuiter.me>
2026-02-24 17:27:54 +0100

    Worked on CI/CD

--- apps/upload-service/deploy.sh
+++ apps/upload-service/deploy.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
-aws cloudformation deploy --stack-name upload-service --template-file upload-service-stack.yaml --capabilities CAPABILITY_NAMED_IAM
+AWS_REGION=eu-west-1
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+REPO=upload-service
 
+if [ -n "${GIT_HASH:-}" ]; then
+  aws cloudformation deploy \
+    --stack-name upload-service \
+    --template-file upload-service-stack.yaml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides UploadServiceImageUri=$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO:$GIT_HASH
+else
+  aws cloudformation deploy \
+    --stack-name upload-service \
+    --template-file upload-service-stack.yaml \
+    --capabilities CAPABILITY_NAMED_IAM
+fi
--- apps/upload-service/publish.sh
+++ apps/upload-service/publish.sh
@@ -8,6 +8,15 @@ aws ecr create-repository --repository-name $REPO --region $AWS_REGION 2>/dev/nu
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-docker buildx build --platform linux/amd64 --provenance=false -t upload-service:latest .
+docker buildx build --platform linux/amd64 --provenance=false -t $REPO:latest .
 docker tag $REPO:latest $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO:latest
+
+if [ -n "${GIT_HASH:-}" ]; then
+  docker tag $REPO:latest $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO:$GIT_HASH
+fi
+
 docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO:latest
+
+if [ -n "${GIT_HASH:-}" ]; then
+  docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO:$GIT_HASH
+fi
