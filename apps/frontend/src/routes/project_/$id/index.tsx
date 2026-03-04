4aedbad05040d62769e599d2b0851854075fe2b0
Noah <m.n.vanos@students.uu.nl>
2026-03-04 10:12:39 +0100

    enhance code view

--- apps/frontend/src/routes/project_/$id/index.tsx
+++ apps/frontend/src/routes/project_/$id/index.tsx
@@ -6,11 +6,16 @@ import { useState } from "react";
 import { API_BASE_URL, BASE_URL, type UploadProject } from "../../../api";
 import {
   CodeViewer,
-  type CodeViewerProps
+  type CodeViewerProps,
 } from "../../../components/CodeViewer";
 import { FileTree } from "../../../components/FileTree";
 import { Container } from "../../../components/ui/Container";
-import { downloadFile, getFileExtension, uploadFilesToFileSystemTree, type FileSystemFile } from "../../../filesystem";
+import {
+  downloadFile,
+  getFileExtension,
+  uploadFilesToFileSystemTree,
+  type FileSystemFile,
+} from "../../../filesystem";
 
 export const Route = createFileRoute("/project_/$id/")({
   component: Project,
@@ -21,17 +26,24 @@ function Project() {
   const navigate = useNavigate();
   const { id } = Route.useParams();
 
-  const { data: project, isPending, error } = useQuery<UploadProject>({
+  const {
+    data: project,
+    isPending,
+    error,
+  } = useQuery<UploadProject>({
     queryKey: ["projects", id],
-    queryFn: () => fetch(`${API_BASE_URL}/upload/projects/${id}`).then((r) => r.json()),
+    queryFn: () =>
+      fetch(`${API_BASE_URL}/upload/projects/${id}`).then((r) => r.json()),
   });
 
-  if(isPending) return <CircularProgress />;
+  if (isPending) return <CircularProgress />;
 
-  if(error) return <div> An error occured {error.message} </div>;
+  if (error) return <div> An error occured {error.message} </div>;
 
   async function onFileClick(file: FileSystemFile) {
-    const content = await downloadFile(`${API_BASE_URL}/upload/projects/${id}/files/${file.downloadId!}`);
+    const content = await downloadFile(
+      `${API_BASE_URL}/upload/projects/${id}/files/${file.downloadId!}`,
+    );
     const fileExtension = getFileExtension(file.name);
 
     if (!fileExtension) {
@@ -42,28 +54,39 @@ function Project() {
   }
 
   async function analyzeFile() {
-    const result = await fetch(`${BASE_URL}/fix/projects/${id}/files/${fileContent?.id}`, {
-      method: "POST",
-    })
+    const result = await fetch(
+      `${BASE_URL}/fix/projects/${id}/files/${fileContent?.id}`,
+      {
+        method: "POST",
+      },
+    )
       .then((res) => res.text())
       .catch((err) => console.error(err));
 
-    localStorage.setItem('TMP_RESULT',JSON.stringify(result));
+    localStorage.setItem("TMP_RESULT", JSON.stringify(result));
     console.log(result);
-    navigate({ to: `/project/$id/results/$file`, params: { id, file: fileContent?.id! } });
+    navigate({
+      to: `/project/$id/results/$file`,
+      params: { id, file: fileContent?.id! },
+    });
   }
 
   return (
     <Container direction="column" overflow="auto">
       <Typography
-          component="h1"
-          variant="h4"
-          sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
-        >
-          Project {project.name} ({project.files.length})
+        component="h1"
+        variant="h4"
+        sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+      >
+        Project {project.name} ({project.files.length})
       </Typography>
-      <Stack direction="row" overflow="auto" margin={2}> 
-        {project.files && <FileTree directory={uploadFilesToFileSystemTree(project.files)} onFileClick={onFileClick} />}
+      <Stack direction="row" overflow="auto" margin={2}>
+        {project.files && (
+          <FileTree
+            directory={uploadFilesToFileSystemTree(project.files)}
+            onFileClick={onFileClick}
+          />
+        )}
         <div>
           {fileContent && (
             <>
@@ -85,6 +108,6 @@ function Project() {
           )}
         </div>
       </Stack>
-    </Container> 
+    </Container>
   );
 }
--- apps/frontend/src/routes/project_/$id/results_/$file/index.tsx
+++ apps/frontend/src/routes/project_/$id/results_/$file/index.tsx
@@ -1,66 +1,71 @@
-import { Box, Link as ButtonLink, CircularProgress, Container, Grid, Paper, Typography, } from '@mui/material';
-import { useQuery } from '@tanstack/react-query';
-import { Link, createFileRoute } from '@tanstack/react-router';
-import { API_BASE_URL } from '../../../../../api';
-import { CodeViewer } from '../../../../../components/CodeViewer';
-import { downloadFile } from '../../../../../filesystem';
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Button, CircularProgress } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { Link, createFileRoute } from "@tanstack/react-router";
+import { API_BASE_URL } from "../../../../../api";
+import CodeDiffViewer from "../../../../../components/CodeDiffViewer";
+import { Container } from "../../../../../components/ui/Container";
+import { downloadFile } from "../../../../../filesystem";
 
-export const Route = createFileRoute('/project_/$id/results_/$file/')({
+export const Route = createFileRoute("/project_/$id/results_/$file/")({
   component: RouteComponent,
-})
+});
+
+function parseMarkdownCodeBlock(str: string) {
+  const normalized = str.replace(/\\n/g, "\n");
+  const match = normalized.match(/```(\w+)?\n([\s\S]*?)```/);
+  if (!match) return null;
+
+  return {
+    language: match[1] || "text",
+    code: match[2],
+  };
+}
 
 function RouteComponent() {
   const { id, file } = Route.useParams();
-  
-  const { data: fileContent, isPending, error } = useQuery<string>({
+
+  const {
+    data: fileContent,
+    isPending,
+    error,
+  } = useQuery<string>({
     queryKey: [id, "files", file],
-    queryFn: () => downloadFile(`${API_BASE_URL}/upload/projects/${id}/files/${file}`),
+    queryFn: () =>
+      downloadFile(`${API_BASE_URL}/upload/projects/${id}/files/${file}`),
   });
 
   const stored = localStorage.getItem("TMP_RESULT") || "";
   const after = JSON.parse(stored);
+  const afterCode = parseMarkdownCodeBlock(after);
 
-  if(isPending) return <CircularProgress />;
+  if (isPending) return <CircularProgress />;
 
-  if(error || !fileContent) return <div> an error occured: {error?.message} </div>; 
+  if (error || !fileContent)
+    return <div> an error occured: {error?.message} </div>;
 
   return (
     <Container>
-      <Link to="/">
-        <ButtonLink
-          component="button"
-          type="button"
-          variant="body2"
-          sx={{ alignSelf: "center" }}
-        >
-          Go back to project
-        </ButtonLink>
-      </Link>
+      <Button
+        component={Link}
+        to={`/project/${id}`}
+        startIcon={<ArrowBackIcon />}
+        variant="outlined"
+        sx={{
+          mb: 3,
+          borderRadius: 2,
+          textTransform: "none",
+          fontWeight: 500,
+        }}
+      >
+        Back to project
+      </Button>
 
-      <Box p={2}>
-      <Grid container spacing={2}>
-        {/* Before Code */}
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3} style={{ padding: "16px", height: "100%" }}>
-            <Typography variant="h6" gutterBottom>
-              Before
-            </Typography>
-            <CodeViewer content={fileContent} language='ts' />
-          </Paper>
-        </Grid>
-
-        {/* After Code */}
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3} style={{ padding: "16px", height: "100%" }}>
-            <Typography variant="h6" gutterBottom>
-              After
-            </Typography>
-            <CodeViewer content={after} language='ts' />
-          </Paper>
-        </Grid>
-      </Grid>
-    </Box>
+      <CodeDiffViewer
+        beforeCode={fileContent}
+        afterCode={afterCode?.code!}
+        language={afterCode?.language!}
+      ></CodeDiffViewer>
     </Container>
   );
 }
-
