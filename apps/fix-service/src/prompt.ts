4567018c5bd976097aae9dfe710155a0798d66f4
Jens Steenmetz <jj.steenmetz@gmail.com>
2026-03-17 23:08:32 +0100

    fix: update LLM prompt

--- apps/fix-service/src/prompt.ts
+++ apps/fix-service/src/prompt.ts
@@ -3,5 +3,7 @@ export const prompt_instructions = `
 The input is a code file and metrics about this code file. 
 Your goal is to improve the software quality of this code. 
 Modify the input code snippet. Try to keep the same level of indenting as the original code.
+IT IS CRUCIAL THAT YOU ONLY FIX THE ISSUES INDICATED BY THE ANALYSIS REPORT.
+Do *NOT* fix any issues that are not identified in the analysis.
 Return the code snippet as a string in your response in markdown format \`\`\`[language] \`\`\`\`.
 `;
